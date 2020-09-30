41fbc3b737f172a47e59cab19540326ddffa257a
Anurag <anurag@pop-os.localdomain>
2020-09-30 22:31:14 +0530

    Added Moving Base Window

--- flappy_bird.py
+++ flappy_bird.py
@@ -123,7 +123,30 @@ class Pipe:
             return True
 
         return False
-        
+
+
+class Base:
+    VELOCITY = 5
+    WIDTH = BASE_IMAGE.get_width()
+    IMAGE = BASE_IMAGE
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+
+    def move(self):
+        self.x1 = self.VELOCITY
+        self.x2 = self.VELOCITY
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+
 def draw_window(window, bird):
     window.blit(BACKGROUND_IMAGE, (0,0))
     bird.draw(window)
