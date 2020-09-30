fcea212de6c87a1445fcd610a9d75d6d9b82464d
Anurag <anurag@pop-os.localdomain>
2020-09-30 22:23:04 +0530

    Defined Collide Method

--- flappy_bird.py
+++ flappy_bird.py
@@ -87,7 +87,6 @@ class Pipe:
     def __init__(self, x):
         self.x = x
         self.height = 0
-        self.gap = 100
 
         self.top = 0
         self.bottom = 0
@@ -95,9 +94,36 @@ class Pipe:
         self.PIPE_BOTTOM = PIPE_IMAGE
 
         self.passed = False
+        self.set_height()
+
+    def set_height(self):
+        self.height = random.randrange(50,450)
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+
+    def move(self):
+        self.x -= self.VELOCITY
+
+    def draw(self, window):
+        window.blit(self.PIPE_TOP, (self.x, self.top))
+        window.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+
+    def collide(self, bird):
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        bottom_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        top_point = bird_mask.overlap(top_mask, top_offset)
+
+        if top_point or bottom_point:
+            return True
+
+        return False
         
-
-
 def draw_window(window, bird):
     window.blit(BACKGROUND_IMAGE, (0,0))
     bird.draw(window)
